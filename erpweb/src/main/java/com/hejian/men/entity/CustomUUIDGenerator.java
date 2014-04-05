e7293d3f379bf4e5a45fc4aaf324de21c5903aff
jason <mhjian_2008@163.com>
2014-04-05 12:27:43 +0800

    自定义主键生成策略，可以定义oracle id主键为varchar

---
+++ erpweb/src/main/java/com/hejian/men/entity/CustomUUIDGenerator.java
@@ -0,0 +1,38 @@
+package com.hejian.men.entity;
+
+import java.io.Serializable;
+import java.util.Properties;
+
+import org.hibernate.HibernateException;
+import org.hibernate.MappingException;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.UUIDGenerator;
+import org.hibernate.type.Type;
+
+public class CustomUUIDGenerator extends UUIDGenerator {
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.id.UUIDGenerator#configure(org.hibernate.type.Type, java.util.Properties, org.hibernate.dialect.Dialect)
+	 */
+	@Override
+	public void configure(Type arg0, Properties arg1, Dialect arg2)
+			throws MappingException {
+		super.configure(arg0, arg1, arg2);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.id.UUIDGenerator#generate(org.hibernate.engine.SessionImplementor, java.lang.Object)
+	 */
+	@Override
+	public Serializable generate(SessionImplementor session, Object object)
+			throws HibernateException {
+		IdEntity entity = (IdEntity) object;
+		if(entity.getId() != null) {
+			return entity.getId();
+		}
+		String uuid = super.generate(session, object).toString().replaceAll("-", "");
+		return uuid;
+	}
+	
+}
